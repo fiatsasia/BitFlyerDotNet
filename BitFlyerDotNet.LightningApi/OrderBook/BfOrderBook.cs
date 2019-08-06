f3e1cb7207a7ccc9e3af08dcee309a3eb6f7718b
Akira <github@fiats.asia>
2019-08-06 15:07:07 +0900

    Improved order book.

--- BitFlyerDotNet.LightningApi/OrderBook/BfOrderBook.cs
+++ BitFlyerDotNet.LightningApi/OrderBook/BfOrderBook.cs
@@ -5,10 +5,11 @@
 
 using System.Linq;
 using System.Collections.Generic;
+using Financial.Extensions;
 
 namespace BitFlyerDotNet.LightningApi
 {
-    public class BfOrderBook
+    public class BfOrderBook : IFxOrderBook
     {
         SortedDictionary<decimal, decimal> _bids { get; } = new SortedDictionary<decimal, decimal>();
         SortedDictionary<decimal, decimal> _asks { get; } = new SortedDictionary<decimal, decimal>();
@@ -21,15 +22,29 @@ namespace BitFlyerDotNet.LightningApi
         public decimal BestAskPrice => _bestAsk.Key;
         public decimal BestAskSize => _bestAsk.Value;
 
+        public decimal TotalBidDepth { get; private set; }
+        public decimal TotalAskDepth { get; private set; }
+
         static readonly KeyValuePair<decimal, decimal> DefaultElement = new KeyValuePair<decimal, decimal>(decimal.Zero, decimal.Zero);
 
         public void Reset(BfBoard orders)
         {
             MidPrice = orders.MidPrice;
-            _bids.Clear(); orders.Bids.ForEach(e => _bids.Add(e.Price, e.Size));
+
+            TotalBidDepth = 0;
+            _bids.Clear(); orders.Bids.ForEach(e =>
+            {
+                _bids.Add(e.Price, e.Size);
+                TotalBidDepth += e.Size;
+            });
             _bestBid = _bids.Last();
 
-            _asks.Clear(); orders.Asks.ForEach(e => _asks.Add(e.Price, e.Size));
+            TotalAskDepth = 0;
+            _asks.Clear(); orders.Asks.ForEach(e =>
+            {
+                _asks.Add(e.Price, e.Size);
+                TotalAskDepth += e.Size;
+            });
             _bestAsk = _asks.First();
         }
 
@@ -48,7 +63,17 @@ namespace BitFlyerDotNet.LightningApi
                     _bids[bid.Price] = bid.Size;
                 }
             }
-            _bestBid = _bids.Count() > 0 ? _bids.Last() : DefaultElement;
+
+            if (_bids.Count() == 0)
+            {
+                _bestBid = DefaultElement;
+                TotalBidDepth = 0;
+            }
+            else
+            {
+                _bestBid = _bids.Last();
+                TotalBidDepth = _bids.Values.Sum();
+            }
 
             foreach (var ask in orders.Asks)
             {
@@ -61,7 +86,17 @@ namespace BitFlyerDotNet.LightningApi
                     _asks[ask.Price] = ask.Size;
                 }
             }
-            _bestAsk = _asks.Count() > 0 ? _asks.First() : DefaultElement;
+
+            if (_asks.Count() == 0)
+            {
+                _bestAsk = DefaultElement;
+                TotalAskDepth = 0;
+            }
+            else
+            {
+                _bestAsk = _asks.First();
+                TotalAskDepth = _asks.Values.Sum();
+            }
         }
 
         public BfOrderBookSnapshot GetSnapshot(int size)
